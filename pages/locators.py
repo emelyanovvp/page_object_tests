c0b46d6ca75ac2b9cafb92799876cbf47d08e735
emelyanovvp <emelyanovvp@mail.ru>
2022-10-29 08:33:42 +0300

    transition on others pages

--- pages/locators.py
+++ pages/locators.py
@@ -4,7 +4,6 @@ class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class LoginPageLocators():
-    LOGIN_URL = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
--- test_login_page.py
+++ test_login_page.py
@@ -1,19 +1,24 @@
 from .pages.login_page import LoginPage
-
+from .pages.main_page import MainPage
 def test_guest_should_go_to_login_page(browser):
     url = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
     page = LoginPage(browser, url)
     page.open()
     page.should_be_login_url()
-
 def test_guest_should_see_login_form(browser):
     url = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
     page = LoginPage(browser, url)
     page.open()
     page.should_be_login_form()
-
 def test_guest_should_see_register_form(browser):
     url = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
     page = LoginPage(browser, url)
     page.open()
     page.should_be_register_form()
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
