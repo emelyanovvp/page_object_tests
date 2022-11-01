dd0a5084842d61aa00d7fc69d631a4655699d0ba
emelyanovvp <emelyanovvp@mail.ru>
2022-11-01 22:21:13 +0300

    transition on others pages

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from selenium.common.exceptions import NoAlertPresentException
 from .locators import ProductPageLocators
 from selenium.webdriver.common.by import By
 import math
@@ -6,7 +7,6 @@ class ProductPage(BasePage):
     def push_button_add_to_basket(self):
         button_add_to_basket = self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_BASKET)
         button_add_to_basket.click()
-
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
@@ -20,20 +20,18 @@ class ProductPage(BasePage):
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
-
     def check_names_of_product(self):
         product_to_choose_element = self.browser.find_element(*ProductPageLocators.GETTING_NAME)
         product_to_choose = product_to_choose_element.text
         message_product_added_to_basket_element = self.browser.find_element(*ProductPageLocators.GETTING_MESSAGE_NAME)
         message_product_added_to_basket = message_product_added_to_basket_element.text
-        assert product_to_choose == message_product_added_to_basket
-
+        assert product_to_choose == message_product_added_to_basket, "Product is not in basket"
     def check_prices_of_product(self):
         price_of_product_element = self.browser.find_element(*ProductPageLocators.GETTING_PRICE)
         price_of_product = price_of_product_element.text
         message_how_much_is_basket_total_element = self.browser.find_element(*ProductPageLocators.GETTING_MESSAGE_PRICE)
         message_how_much_is_basket_total = message_how_much_is_basket_total_element.text
-        assert price_of_product == message_how_much_is_basket_total
+        assert price_of_product == message_how_much_is_basket_total, "Price of product is not equal basket total"
 
 
 
--- test_product_page.py
+++ test_product_page.py
@@ -1,12 +1,22 @@
-from selenium.common.exceptions import NoAlertPresentException
+import pytest
 from .pages.product_page import ProductPage
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear2019"
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser,link):
     page = ProductPage(browser, link)
     page.open()
     page.push_button_add_to_basket()
     page.solve_quiz_and_get_code()
     page.check_names_of_product()
     page.check_prices_of_product()
-
+if __name__ == '__main__':
+    pytest.main()
